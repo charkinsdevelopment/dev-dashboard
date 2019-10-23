b15a7046fab1525855fac059a983e080af20bcba
Cory Harkins <cory.harkins@gmail.com>
2019-10-23 13:34:23 -0400

    fixing problems with loading bulma

--- DevDashboardProject/DevDashboardProject/App_Start/BundleConfig.cs
+++ DevDashboardProject/DevDashboardProject/App_Start/BundleConfig.cs
@@ -19,9 +19,6 @@ namespace DevDashboardProject
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
             bundles.Add(new ScriptBundle("~/bundles/vue").Include(
                      "~/Scripts/vue.js"));
 
