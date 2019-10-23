549bd02b9eeb14becf5b2c6024043ae83915c6d8
Cory Harkins <cory.harkins@gmail.com>
2019-10-23 13:14:34 -0400

    initial commit for base project

---
+++ DevDashboard/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DevDashboard.MvcApplication" Language="C#" %>
---
+++ DevDashboard/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DevDashboard.Startup))]
+namespace DevDashboard
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
