d4097698a3e05b23a9737b5f8c937cb94a82a7fe
Cory Harkins <cory.harkins@gmail.com>
2019-10-23 13:22:11 -0400

    base checkin

---
+++ DevDashboardProject/DevDashboardProject/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace DevDashboardProject
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ DevDashboardProject/DevDashboardProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DevDashboardProject.MvcApplication" Language="C#" %>
